aac489988ae1582cbd593594bb24817e9727ef0e
Margarita <margarita72973@gmail.com>
2018-07-09 11:26:56 +0300

    added React-Router and provided more functionality for components

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
+import Home from '../../features/home/HomePage';
+import EventDetailed from '../../features/event/EventDetailed/EventDetailedPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard';
+import UserDetailed from '../../features/userDetailed/UserDetailedPage';
+import SettingsDashboard from '../../features/settings/SettingsDashboard';
+import EventForm from '../../features/event/EventForm/EventForm';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <Container className="main">
-          <EventDashboard />
-        </Container>
+        <Switch>
+          <Route exact path='/' component={ Home } />
+        </Switch>
+
+        <Route path='/(.+)'
+               render={()=>(
+                  <div>
+                  <NavBar />
+                  <Container className="main">
+                    <Switch>
+                      <Route path='/events' component={ EventDashboard } />
+                      <Route path='/event/:id' component={ EventDetailed } />
+                      <Route path='/people' component={ PeopleDashboard } />
+                      <Route path='/profile/:id' component={ UserDetailed } />
+                      <Route path='/settings' component={ SettingsDashboard } />
+                      <Route path='/createEvent' component={ EventForm } />  
+                    </Switch>
+                  </Container>
+                </div>
+               )} 
+        />
       </div>
     );
   }
---
+++ src/features/userDetailed/UserDetailedPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      <h1>User Detailed</h1>
+    </div>
+  )
+}
