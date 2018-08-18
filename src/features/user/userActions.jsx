a0a9b9eedf351565e16d9c8cb49eac9bf346d3de
Margarita <margarita72973@gmail.com>
2018-08-18 18:35:22 +0300

    starting work on upload images functionality

--- src/features/user/userActions.jsx
+++ src/features/user/userActions.jsx
@@ -16,3 +16,28 @@ export const updateProfile = (user) =>
             console.log(err)
         }
     }
+
+
+export const uploadProfileImage = (file, fileName) => 
+    async (dispatch, getState, {getFirebase, getFirestore}) => {
+        const firebase = getFirebase();
+        const firestore = getFirestore();
+        const user = firebase.auth().currentUser;
+        const path = `${user.uid}/user_images`;
+        const options = {
+            name: fileName
+        }
+        try {
+            //upload file to firebase storage
+            let uploadedFile = await firebase.uploadFile(path, file, null, options);
+            let downloadURL = await uploadedFile.uploadTaskSnapshot.downloadURL;
+            let userDoc = await firestore.get(`users/${user.uid}`);
+            //get url of image
+            //get userdoc 
+            //check if user has photo, if not update profile with new image
+            //add the new photo to photos collection
+        } catch(err) {
+            console.log('err', err)
+        }
+
+    }
